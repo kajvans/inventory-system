9afd5eddcb7da2524b01854c8a7b0c082883367b
kaj van schalkwijk <kajvanschalkwijk@gmail.com>
2022-08-13 21:50:06 +0200

    master

---
+++ backend/models/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: {type: String, required: true, unique: true},
+  password: {type: String, required: true, minlength: process.env.MIN_PASSWORD_LENGTH},
+  isAdmin: {type: Boolean, default: false},
+});
+
+module.exports = User = mongoose.model("user", userSchema);
