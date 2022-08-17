e08b88806cce7f78f55fcbc4d9135c319bc5773e
kaj van schalkwijk <kajvanschalkwijk@gmail.com>
2022-08-17 13:46:33 +0200

    master

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -11,15 +11,15 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) console.log(error);
+    if (err) return console.log(err);
     console.log("Connected!");
 });
 
 router.post("/search", (req, res) => {
-    const { search } = req.body;
+    const { name } = req.body;
     return new Promise((resolve, reject) => {
         var sql = `SELECT name, price FROM products WHERE name LIKE ?`;
-        connection.query(sql, ["%" + search + "%"], (err, result) => {
+        connection.query(sql, ["%" + name + "%"], (err, result) => {
             if (err) reject(err);
             else if (result.length === 0) reject("No results found");
             resolve(result);
@@ -28,8 +28,8 @@ router.post("/search", (req, res) => {
     }).then(result => {
         if (result.length === 1) {
             return new Promise((resolve, reject) => {
-                var sql = `SELECT products.name AS name, products.price AS price, products.stock AS stock, products.barcode AS barcode, products.category AS category, products.location AS location, products.expire AS expire, history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date FROM products INNER JOIN history ON history.products_id = products.id WHERE name = ? ORDER BY history.date DESC LIMIT 1`;
-                var inserts = [search];
+                var sql = `SELECT products.name AS name, products.price AS price, products.stock AS stock, products.barcode AS barcode, products.category AS category, products.location AS location, products.expire AS expire, history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date FROM products INNER JOIN history ON history.products_name = products.name WHERE name = ? ORDER BY history.date DESC LIMIT 1`;
+                var inserts = [name];
                 connection.query(sql, inserts, (err, result) => {
                     if (err) reject(err);
                     else resolve(result);
@@ -50,10 +50,10 @@ router.post("/search", (req, res) => {
 })
 
 router.post("/select", (req, res) => {
-    const { search } = req.body;
+    const { name } = req.body;
     return new Promise((resolve, reject) => {
-        var sql = `SELECT history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date, products.name AS name, products.price AS price, products.stock AS stock, products.barcode AS barcode, products.category AS category, products.location AS location, products.expire AS expire FROM products INNER JOIN history ON history.products_id = products.id WHERE name = ? ORDER BY history.date DESC LIMIT 1`;
-        var inserts = [search];
+        var sql = `SELECT history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date, products.name AS name, products.price AS price, products.stock AS stock, products.barcode AS barcode, products.category AS category, products.location AS location, products.expire AS expire FROM products INNER JOIN history ON history.products_name = products.name WHERE name = ? ORDER BY history.date DESC LIMIT 1`;
+        var inserts = [name];
         connection.query(sql, inserts, (err, result) => {
             if (err) reject(err);
             else if (result.length === 0) reject("No results found");
@@ -69,7 +69,7 @@ router.post("/select", (req, res) => {
 router.post("/history", (req, res) => {
     const { search } = req.body;
     return new Promise((resolve, reject) => {
-        var sql = `SELECT history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date FROM history INNER JOIN products ON history.products_id = products.id WHERE name = ? ORDER BY history.date DESC`;
+        var sql = `SELECT history.sold AS sold, history.received AS received, history.deleted AS deleted, DATE_FORMAT(history.date, '%e/%c/%Y') AS date FROM history INNER JOIN products ON history.products_name = products.name WHERE name = ? ORDER BY history.date DESC`;
         var inserts = [search];
         connection.query(sql, inserts, (err, result) => {
             if (err) reject(err);
@@ -83,64 +83,58 @@ router.post("/history", (req, res) => {
     });
 })
 
+//change the stock of multiple products with a for loop
 router.post("/sold", (req, res) => {
-    for (var i = 0; i < req.body.length; i++) {
-        return new Promise((resolve, reject) => {
-            var sql = `SELECT id FROM products WHERE name = ?`;
-            var inserts = [req.body[i].search];
+    return new Promise((resolve, reject) => {
+        for(var i = 0; i < req.body.length; i++) {
+            const { name, sold } = req.body[i];
+            var sql = `UPDATE products SET stock = stock - ? WHERE name = ?`;
+            var inserts = [sold, name];
             connection.query(sql, inserts, (err, result) => {
                 if (err) reject(err);
-                else if (result.length === 0) reject("No results found");
-                resolve(result);
-            });
-        }).then(result => {
-            var sql = 'UPDATE `products` SET stock = stock - ? WHERE id = ?; UPDATE `history` SET sold = sold + ? WHERE products_id = ? AND date = CURRENT_DATE'; //gives error but not when used in admin screen of xampp
-            var inserts = [req.body[i].number, result[0].id, req.body[i].number, result[0].id];
-            return new Promise((resolve, reject) => {
-                connection.query(sql, inserts, (err, result) => {
-                    if (err) reject(err);
-                    else resolve(result);
+                else {
+                    var sql = `UPDATE history SET sold = sold + ? WHERE products_name = ?`;
+                    var inserts = [sold, name];
+                    connection.query(sql, inserts, (err, result) => {
+                        if (err) reject(err);
+                        else resolve(result);
+                    });
                 }
-                );
-            }).then(result => {
-                res.json(result);
-            }).catch(err => {
-                res.json(err);
             });
-        }).catch(err => {
-            res.json(err);
-        });
-    }
+        }
+        resolve("Success");
+    }).then(result => {
+        res.json(result);
+    }).catch(err => {
+        res.json(err);
+    });
 })
 
 router.post("/received", (req, res) => {
-    for (var i = 0; i < req.body.length; i++) {
-        return new Promise((resolve, reject) => {
-            var sql = `SELECT id FROM products WHERE name = ?`;
-            var inserts = [req.body[i].search];
+    return new Promise((resolve, reject) => {
+        for(var i = 0; i < req.body.length; i++) {
+            const { name, received } = req.body[i];
+            var sql = `UPDATE products SET stock = stock + ? WHERE name = ?`;
+            var inserts = [received, name];
             connection.query(sql, inserts, (err, result) => {
                 if (err) reject(err);
-                else if (result.length === 0) reject("No results found");
-                resolve(result);
-            });
-        }).then(result => {
-            var sql = 'UPDATE `products` SET stock = stock + ? WHERE id = ?; UPDATE `history` SET received = received + ? WHERE products_id = ? AND date = CURRENT_DATE';//gives error but not when used in admin screen of xampp
-            var inserts = [req.body[i].number, result[0].id, req.body[i].number, result[0].id];
-            return new Promise((resolve, reject) => {
-                connection.query(sql, inserts, (err, result) => {
-                    if (err) reject(err);
-                    else resolve(result);
+                else {
+                    var sql = `UPDATE history SET received = received + ? WHERE products_name = ?`;
+                    var inserts = [received, name];
+                    connection.query(sql, inserts, (err, result) => {
+                        if (err) reject(err);
+                        else resolve(result);
+                    });
                 }
-                );
-            }).then(result => {
-                res.json(result);
-            }).catch(err => {
-                res.json(err);
             });
-        }).catch(err => {
-            res.json(err);
-        });
-    }
+        }
+        resolve("Success");
+    }).then(result => {
+        res.json(result);
+    }).catch(err => {
+        res.json(err);
+    });
 })
+
 
 module.exports = router;
