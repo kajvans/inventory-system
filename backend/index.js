7d80b6c995245f37f64a7feb486c9b3bfa97876a
Julian Alberts <julian.alberts@ewe.net>
2022-08-20 11:14:01 +0200

    Added basic migration

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,21 @@ const express = require("express");
 const dailyUpdate = require("./dailyUpdate");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const mysql = require("mysql");
+const migration = require("mysql-migrations");
 require("dotenv").config();
+
+const connection = mysql.createPool({
+    connectionLimit : 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+migration.init(connection, __dirname + '/migrations', function() {
+    console.log("finished running migrations");
+});
 
 // set up express
 const app = express();
