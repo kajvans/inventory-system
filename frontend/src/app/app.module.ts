60b948c43b59f55aa1727b50fc740de3988fe457
kaj van schalkwijk <kajvanschalkwijk@gmail.com>
2022-09-06 18:51:32 +0200

    master

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminDashboardComponent,
   ],
   imports: [
     BrowserModule,
