276bd70f599e530fb85548ff8a74ab3b9f091743
kaj van schalkwijk <kajvanschalkwijk@gmail.com>
2022-09-08 18:53:11 +0200

    master

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AdminDashboardComponent,
+    ItemComponent,
+    ItemListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin', component: AdminDashboardComponent },
+      { path: 'item', component: ItemComponent },
+      { path: 'item-list', component: ItemListComponent },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
